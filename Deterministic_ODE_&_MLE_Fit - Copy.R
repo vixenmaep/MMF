65d266b863ab79694e4b1814243fff0230005651
vixenmaep <vickymae030603@gmail.com>
2026-06-23 20:41:53 +0200

    have tried to model housing thing, its not working, pls help

--- Deterministic_ODE_&_MLE_Fit - Copy.R
+++ Deterministic_ODE_&_MLE_Fit - Copy.R
@@ -3,6 +3,7 @@ library(deSolve)
 library(ggplot2)
 library(ellipse)
 library(tidyr)
+library(chron)
 
 is_positive_scalar <- function(x) {
   is.numeric(x) && length(x) == 1 && !is.na(x) && x > 0
@@ -46,125 +47,135 @@ obsDat[is.na(obsDat)] <- 0
 cat("\nObserved daily incidence:\n")
 print(obsDat)
 
+
 betaMixprior <- 1.8
 betaNightEprior <- 0
 betaNightAprior <-1.5
-betaA <- function(Time) {ifelse(Time >= "08:00:00" & Time <= "18:30:00", betaMixprior, betaNightAprior)}
-betaE <- function(Time) {ifelse(Time >= "08:00:00" & Time <= "18:30:00", betaMixprior, betaNightEprior)}
-disease_params <- function(
-    beta   = betafunc,
-    pA     = 0.4,   
-    gammaA = 1,     
-    gammaS = 1,     
-    N      = 34     
-) {
-  stopifnot("beta must be a positive scalar" = is_positive_scalar(beta))
-  return(as.list(environment()))
-}
+#disease_params <- function(
+    # Inside your SIR derivative function:
+ #   gammaA = 1,     
+  #  gammaS = 1,     
+#    N      = 46     
+#) {
+ # stopifnot("beta must be a positive scalar" = is_positive_scalar(beta))
+  #return(as.list(environment()))
+#}
 
-index_case <- data[data$Infected.by == "Started", ][1, ]
-I0_IS <- as.integer(index_case$Location == 1)
-I0_IA <- as.integer(index_case$Location == 0)
-N0    <- nrow(data)
+index_cases <- data[data$Infected.by == "Started", ]
+I0_IE <- sum(data$Location == 1 & data$Infected.by == "Started") #sum(as.integer(index_case$Location == 1))
+I0_IA <- sum(data$Location == 0 & data$Infected.by == "Started")
+#I0_IA <- sum(as.integer(index_case$Location == 0))
+S0_E <- sum(data$Location) - I0_IE
+N0    <- 42
+S0_A <- N0 - S0_E - I0_IA
+N_A <- 25 
+N_E <- 17
 
-pop.siar <- c(
-  S  = N0 - I0_IA - I0_IS,
+pop.ssiirr <- c(
+  SE  = S0_E,
+  SA = S0_A,
   IA = I0_IA,
-  IS = I0_IS,
-  R  = 0
+  IE = I0_IE,
+  RA  = 0,
+  RE = 0
 )
 
 cat("\nInitial conditions:\n")
-print(pop.siar)
+print(pop.ssiirr)
 
 # assumed beta
 values <- c(
-  beta   = 1.8,
-  pA     = 0.4,
   gammaA = 1,
-  gammaS = 1,
+  gammaE = 1,
   N      = N0
 )
 
 cat("\nParameter values:\n")
 print(values)
 
-# R0 = beta when gammaA = gammaS = 1
-R0_value <- with(as.list(values), {
-  beta * (pA / gammaA + (1 - pA) / gammaS)
-})
+R0_prior <- betaMixprior
 cat("\nBasic Reproduction Number (R0):", round(R0_value, 3), "\n")
 
-siar <- function(t, y, parms) {
+ssiirr <- function(t, y, parms) {
   with(c(as.list(y), parms), {
+    beta_M  <- ifelse((t >= as.integer(t) + 0.3328) & (t <= as.integer(t) + 0.77071), betaMixprior, 0)
+    beta_NA <- ifelse((t >= as.integer(t) + 0.3328) & (t <= as.integer(t) + 0.77071), 0, betaNightAprior)
+    beta_NE <- ifelse((t >= as.integer(t) + 0.3328) & (t <= as.integer(t) + 0.77071), 0, betaNightEprior)
     
-    lambda         <- beta * (IA + IS) / N
-    new_infections <- lambda * S
+    lambdaA        <- (beta_M*(IA + IE)/N0 + beta_NA*IA/N_A)*SA
+    lambdaE       <- (beta_M*(IA + IE)/N0 + beta_NE*IE/N_E)*SE
+    new_infectionsA <- lambdaA
+    new_infectionsE <- lambdaE
     
-    dSdt  <- -new_infections
-    dIAdt <-  pA      * new_infections - gammaA * IA
-    dISdt <- (1 - pA) * new_infections - gammaS * IS
-    dRdt  <-  gammaA * IA + gammaS * IS
+    dSAdt  <- -lambdaA
+    dSEdt <- -lambdaE
+    dIAdt <-  new_infectionsA - gammaA * IA
+    dIEdt <-  new_infectionsE - gammaE * IE
+    dRAdt  <-  gammaA * IA 
+    dREdt  <-  gammaE * IE 
     
-    return(list(c(dSdt, dIAdt, dISdt, dRdt)))
+    return(list(c(dSAdt, dSEdt, dIAdt, dIEdt, dRAdt, dREdt)))
   })
 }
 
-siar_CI <- function(t, y, parms) {
-  with(c(as.list(y), parms), {
+# TODO: did we even plot confidence intervals? Do we need them?
+#ssiirr_CI <- function(t, y, parms) {
+ # with(c(as.list(y), parms), {
     
-    lambda         <- beta * (IA + IS) / N
-    new_infections <- lambda * S
+  #  lambda         <- beta * (IA + IS) / N
+   # new_infections <- lambda * S
     
-    dSdt  <- -new_infections
-    dIAdt <-  pA      * new_infections - gammaA * IA
-    dISdt <- (1 - pA) * new_infections - gammaS * IS
-    dRdt  <-  gammaA * IA + gammaS * IS
-    dCIdt <-  new_infections             # cumulative incidence tracker
+    #dSdt  <- -new_infections
+  #  dIAdt <-  pA      * new_infections - gammaA * IA
+   # dISdt <- (1 - pA) * new_infections - gammaS * IS
+  #  dRdt  <-  gammaA * IA + gammaS * IS
+   # dCIdt <-  new_infections             # cumulative incidence tracker
     
-    return(list(c(dSdt, dIAdt, dISdt, dRdt, dCIdt)))
-  })
-}
+ #   return(list(c(dSdt, dIAdt, dISdt, dRdt, dCIdt)))
+#  })
+#}
 
 # DETERMINISTIC MODEL — ASSUMED BETA = 1.8
 
-time.out <- seq(0, 10, by = 0.1)
+time.out <- seq(0, 5, by = 0.02083)
 
-ts.siar <- data.frame(lsoda(
-  y     = pop.siar,
+ts.ssiirr <- data.frame(lsoda(
+  y     = pop.ssiirr,
   times = time.out,
-  func  = siar,
+  func  = ssiirr,
   parms = values
 ))
 
-cat("\nModel output at day 4:\n");  print(subset(ts.siar, time == 4))
-cat("\nModel output at day 10:\n"); print(subset(ts.siar, time == 10))
+cat("\nModel output at day 2:\n");  print(subset(ts.ssiirr, time == 2))
+cat("\nModel output at day 5:\n"); print(subset(ts.ssiirr, time == 5))
 
-
+#TODO: split data by location for this thing
 daily_obs <- aggregate(Infection_number ~ Day + Status, data = data, FUN = length)
 names(daily_obs)[3] <- "Count"
 cat("\nObserved daily incidence by type:\n")
 print(daily_obs)
 
-
+# TODO: Redo compartments
 # Plotting all four compartments over time
-ts_long <- pivot_longer(ts.siar,
-                        cols      = c("S", "IA", "IS", "R"),
+ts_long <- pivot_longer(ts.ssiirr,
+                        cols      = c("SA","SE", "IA", "IE", "RA", "RE"),
                         names_to  = "Compartment",
                         values_to = "Count")
 
 ggplot(ts_long, aes(x = time, y = Count, colour = Compartment)) +
   geom_line(linewidth = 1.1) +
   scale_colour_manual(
-    values = c(S = "steelblue", IA = "orange", IS = "red", R = "forestgreen"),
-    labels = c(S  = "Susceptible",
-               IA = "Infectious (Asymptomatic)",
-               IS = "Infectious (Symptomatic)",
-               R  = "Recovered")
+    values = c(SA = "steelblue", SE = "magenta", IA = "orange", IE = "red", RA = "forestgreen", RE = "black"),
+    labels = c(SA  = "Susceptible living at AIMS",
+               SE = "Susceptible living at Empire",
+               IA = "Infectious (living at AIMS)",
+               IE = "Infectious (living at Empire)",
+               RA  = "Recovered at AIMS", 
+               RE = "Recovered at Empire")
   ) +
   xlab("Days since index case (15 June 2026)") +
   ylab("Number of individuals") +
-  ggtitle("SIAR Deterministic Model (assumed beta = 1.8)",
+  ggtitle("ssiirr Deterministic Model (assumed beta = 1.8)",
           subtitle = paste0("R0 = ", round(R0_value, 2),
                             "  |  pA = ", round(values["pA"], 2))) +
   theme_bw(base_size = 13) +
@@ -174,32 +185,32 @@ ggplot(ts_long, aes(x = time, y = Count, colour = Compartment)) +
 cumulative_obs <- aggregate(Infection_number ~ Day, data = data, FUN = length)
 
 ggplot() +
-  geom_line(data = ts.siar,
-            aes(x = time, y = IA + IS, colour = "Model (IA+IS)"),
+  geom_line(data = ts.ssiirr,
+            aes(x = time, y = IA + IS, colour = "Model (IA+IE)"),
             linewidth = 1.1) +
   geom_point(data = cumulative_obs,
              aes(x = Day, y = Infection_number, colour = "Observed daily cases"),
              size = 3) +
-  scale_colour_manual(values = c("Model (IA+IS)"        = "red",
+  scale_colour_manual(values = c("Model (IA+IE)"        = "red",
                                  "Observed daily cases" = "black")) +
   xlab("Days since index case") +
   ylab("Number of active infectious individuals") +
-  ggtitle("SIAR Model (assumed beta) vs Observed Outbreak Incidence") +
+  ggtitle("ssiirr Model (assumed beta) vs Observed Outbreak Incidence") +
   theme_bw(base_size = 13) +
   theme(legend.position = "bottom", legend.title = element_blank())
 
 # NEGATIVE LOG-LIKELIHOOD (POISSON)
-
+# TODO: change this to include all original populations
 initialize_pop_CI <- function(N = N0, IA0 = I0_IA, IS0 = I0_IS) {
   c(S = N - IA0 - IS0, IA = IA0, IS = IS0, R = 0, CI = 0)
 }
 
 simEpidemic <- function(parms = disease_params(),
-                        tseq  = seq(0, 4, by = 0.01)) {
+                        tseq  = seq(0, 5, by = 0.02083)) {
   init <- initialize_pop_CI(N = parms$N)
-  as.data.frame(lsoda(init, tseq, siar_CI, parms = parms))
+  as.data.frame(lsoda(init, tseq, ssiirr_CI, parms = parms))
 }
-
+# TODO: need to figure out how to change this to simulate per half hour
 get_daily_incidence <- function(simDat, days = 0:4) {
   ci_vals   <- sapply(days, function(d) simDat$CI[which.min(abs(simDat$time - d))])
   daily_inc <- c(ci_vals[1], diff(ci_vals))
@@ -207,7 +218,7 @@ get_daily_incidence <- function(simDat, days = 0:4) {
 }
 
 nllikelihood <- function(parms = disease_params(), obsDat = obsDat) {
-  sim    <- simEpidemic(parms = parms, tseq = seq(0, max(obsDat$Day), by = 0.01))
+  sim    <- simEpidemic(parms = parms, tseq = seq(0, max(obsDat$Day)+1, by = 0.02083))
   pred   <- get_daily_incidence(sim, days = obsDat$Day)
   lambda <- pmax(pred$predicted, 1e-10)   # avoid log(0)
   nlls   <- -dpois(round(obsDat$new_cases), lambda = lambda, log = TRUE)
@@ -215,7 +226,7 @@ nllikelihood <- function(parms = disease_params(), obsDat = obsDat) {
 }
 
 # Test: NLL at assumed beta = 1.8
-cat("\nNLL at assumed beta = 1.8:", round(nllikelihood(disease_params(), obsDat), 4), "\n")
+cat("\nNLL at assumed beta = 1.8:", round(nllikelihood(disease_params(), obsDat), 5), "\n")
 
 # Fitting beta on log scale 
 subsParms <- function(fit.params, fixed.params = disease_params()) {
@@ -251,7 +262,7 @@ optim.vals <- optim(
   control      = list(trace = 3, maxit = 150),
   method       = "SANN"
 )
-cat("\nSANN result — beta =", round(exp(optim.vals$par), 4), "\n")
+cat("\nSANN result — beta =", round(exp(optim.vals$par), 5), "\n")
 
 # deterministic
 optim.vals <- optim(
@@ -268,9 +279,9 @@ MLEfits  <- optim.vals$par
 beta.MLE <- exp(MLEfits["log_beta"])
 
 cat("\n--- MLE Results ---\n")
-cat("Fitted beta         :", round(beta.MLE, 4), "\n")
-cat("R0 = beta           :", round(beta.MLE, 4), "\n")
-cat("NLL at MLE          :", round(optim.vals$value, 4), "\n")
+cat("Fitted beta         :", round(beta.MLE, 5), "\n")
+cat("R0 = beta           :", round(beta.MLE, 5), "\n")
+cat("NLL at MLE          :", round(optim.vals$value, 5), "\n")
 cat("Convergence (0=good):", optim.vals$convergence, "\n")
 
 # Confidence Intervals
@@ -281,15 +292,16 @@ se_log_beta     <- sqrt(diag(fisherInfMatrix))
 ci_log_beta <- MLEfits["log_beta"] + c(-1, 1) * 1.96 * se_log_beta
 ci_beta     <- exp(ci_log_beta)
 
-cat("\n95% CI for beta / R0: [", round(ci_beta[1], 4), ",", round(ci_beta[2], 4), "]\n")
+cat("\n95% CI for beta / R0: [", round(ci_beta[1], 5), ",", round(ci_beta[2], 5), "]\n")
 
 # Simulate with MLE beta and with assumed beta = 1.8 for comparison
 fitParms  <- subsParms(MLEfits, disease_params())
-fitDat    <- simEpidemic(parms = fitParms,      tseq = seq(0, 4, by = 0.01))
-assumeDat <- simEpidemic(parms = disease_params(), tseq = seq(0, 4, by = 0.01))
+fitDat    <- simEpidemic(parms = fitParms,      tseq = seq(0, 5, by = 0.02083))
+assumeDat <- simEpidemic(parms = disease_params(), tseq = seq(0, 5, by = 0.02083))
 
-fit_daily    <- get_daily_incidence(fitDat,    days = 0:4)
-assumed_daily <- get_daily_incidence(assumeDat, days = 0:4)
+# TODO: double check how days works
+fit_daily    <- get_daily_incidence(fitDat,    days = 0:5)
+assumed_daily <- get_daily_incidence(assumeDat, days = 0:5)
 
 ggplot() +
   geom_col(data = obsDat,
@@ -310,7 +322,7 @@ ggplot() +
   labs(
     x        = "Days since index case (15 June 2026)",
     y        = "New cases per day",
-    title    = "SIAR Model — Assumed Beta vs MLE Fit",
+    title    = "ssiirr Model — Assumed Beta vs MLE Fit",
     subtitle = paste0("Assumed beta = 1.8  |  MLE beta = ", round(beta.MLE, 3),
                       "  |  R0 = ",         round(beta.MLE, 3),
                       "  |  95% CI: [",     round(ci_beta[1], 3),
